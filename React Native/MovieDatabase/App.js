ff4bc430b6cb9525f834ca6c2762c8b97ed0185c
Daniel Bahrami <daniel@bahrami.dk>
2022-09-12 01:48:10 +0200

    Update App.js

--- React Native/MovieDatabase/App.js
+++ React Native/MovieDatabase/App.js
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>App</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default App;
