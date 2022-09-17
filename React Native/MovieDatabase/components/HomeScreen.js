d340ebbc8a680ff67bc27148ce4c38b3eb039342
Daniel Bahrami <daniel@bahrami.dk>
2022-09-17 22:54:55 +0200

    Add styling to home screen

--- React Native/MovieDatabase/components/HomeScreen.js
+++ React Native/MovieDatabase/components/HomeScreen.js
@@ -1,14 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import Header from "./Header";
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Header title={"Movies"}></Header>
       <Text>HomeScreen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    marginTop: 60,
+  },
+});
 
 export default HomeScreen;
