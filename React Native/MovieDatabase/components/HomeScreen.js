d6f0d460bfec108ed346f456eb6044ea05d4e07f
Daniel Bahrami <daniel@bahrami.dk>
2022-09-17 22:34:48 +0200

    Update HomeScreen.js

--- React Native/MovieDatabase/components/HomeScreen.js
+++ React Native/MovieDatabase/components/HomeScreen.js
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Header from "./Header";
 
 const HomeScreen = () => {
   return (
     <View>
+        <Header title={"Movies"}></Header>
       <Text>HomeScreen</Text>
     </View>
   );
