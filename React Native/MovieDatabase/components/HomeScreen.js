1d6165324e557c781c9d4f80c9568a3c5663d37b
Daniel Bahrami <daniel@bahrami.dk>
2022-09-18 00:04:24 +0200

    Import FlatList

--- React Native/MovieDatabase/components/HomeScreen.js
+++ React Native/MovieDatabase/components/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import React from "react";
 import Header from "./Header";
 
