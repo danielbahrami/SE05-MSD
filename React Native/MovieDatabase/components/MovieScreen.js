cf49f31d90ed5e6e562319e504b95729279f833d
Daniel Bahrami <daniel@bahrami.dk>
2022-09-17 21:18:34 +0200

    Reformat

--- React Native/MovieDatabase/components/MovieScreen.js
+++ React Native/MovieDatabase/components/MovieScreen.js
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const MovieScreen = () => {
   return (
     <View>
       <Text>MovieScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default MovieScreen
+export default MovieScreen;
