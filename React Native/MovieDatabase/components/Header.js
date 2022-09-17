cafe61cca4f6cceec837d7d7a68b324e256a8d47
Daniel Bahrami <daniel@bahrami.dk>
2022-09-17 22:34:33 +0200

    Update Header.js

--- React Native/MovieDatabase/components/Header.js
+++ React Native/MovieDatabase/components/Header.js
@@ -4,11 +4,19 @@ import React from "react";
 const Header = ({ title }) => {
   return (
     <View>
-      <Text>{title}</Text>
+      <Text style={styles.header}>{title}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    fontFamily: "Arial",
+    fontSize: 40,
+    fontWeight: "bold",
+    marginTop: 60,
+    marginLeft: 130,
+  },
+});
 
 export default Header;
