fb82904f9b75b4e3579082a0918025332e2af2d1
Daniel Bahrami <daniel@bahrami.dk>
2022-09-18 00:08:48 +0200

    Remove unused imports

--- React Native/MovieDatabase/App.js
+++ React Native/MovieDatabase/App.js
@@ -1,6 +1,5 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import React from "react";
-import Header from "./components/Header";
 import HomeScreen from "./components/HomeScreen";
 
 const App = () => {
