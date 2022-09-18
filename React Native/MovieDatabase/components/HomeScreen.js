3ec5a4e936f1246fe6342ed29d6184e4f43a5c4e
Daniel Bahrami <daniel@bahrami.dk>
2022-09-18 17:42:50 +0200

    Update HomeScreen.js

--- React Native/MovieDatabase/components/HomeScreen.js
+++ React Native/MovieDatabase/components/HomeScreen.js
@@ -5,11 +5,6 @@ import React from "react";
 const HomeScreen = ({ navigation }) => {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    console.log("Fetching movies with API key: " + process.env.API_KEY);
-    fetchMovies();
-  }, []);
-
   function fetchMovies() {
     fetch(
       `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
@@ -18,18 +13,37 @@ const HomeScreen = ({ navigation }) => {
       .then((data) => setData(data.results));
   }
 
+  useEffect(() => {
+    console.log("Fetching movies with API key: " + process.env.API_KEY);
+    fetchMovies();
+  }, []);
+
+  const Movie = ({ navigation, title, movieId }) => (
+    <View>
+      <Text
+        onPress={() =>
+          navigation.navigate("Details", {
+            movieId,
+          })
+        }
+        style={styles.title}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+
   const renderItem = ({ item }) => (
-    <Item navigation={navigation} movieId={item.id} title={item.title} />
+    <Movie navigation={navigation} movieId={item.id} title={item.title} />
   );
 
   return (
     <View style={styles.container}>
-      <Header title={"Movies"} size={30} color={"black"}></Header>
-      <FlatList>
+      <FlatList
         data={data}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
-      </FlatList>
+      />
     </View>
   );
 };
