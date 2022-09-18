89cb46c981823d63a504d45eeb7ef63511a8fd7a
Daniel Bahrami <daniel@bahrami.dk>
2022-09-18 16:25:19 +0200

    Revert "Rename component"
    This reverts commit 34e87e209f6155da90a6bfb2d1f8cac6ddb1948b.

--- React Native/MovieDatabase/components/MovieScreen.js
+++ React Native/MovieDatabase/components/MovieScreen.js
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const DetailScreen = () => {
+const MovieScreen = () => {
   return (
     <View>
-      <Text>DetailScreen</Text>
+      <Text>MovieScreen</Text>
     </View>
   );
 };
 
-export default DetailScreen;
+export default MovieScreen;
