25485f096e015a0f1020faaa0b87349d34a6bcd6
Daniel Bahrami <daniel@bahrami.dk>
2022-09-18 23:35:04 +0200

    Update imports

--- React Native/MovieDatabase/components/MovieDetails.js
+++ React Native/MovieDatabase/components/MovieDetails.js
@@ -1,5 +1,6 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
+import { useEffect, useState } from "react";
 
 const MovieDetails = () => {
   return (
