c26b93f909397945b869cbd98ac7f387fe2ecf67
Daniel Bahrami <daniel@bahrami.dk>
2022-09-18 16:28:27 +0200

    Update App.js imports

--- React Native/MovieDatabase/App.js
+++ React Native/MovieDatabase/App.js
@@ -1,6 +1,9 @@
 import { View, StyleSheet } from "react-native";
 import React from "react";
 import HomeScreen from "./components/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MovieScreen from "./components/MovieScreen";
 
 const App = () => {
   return (
