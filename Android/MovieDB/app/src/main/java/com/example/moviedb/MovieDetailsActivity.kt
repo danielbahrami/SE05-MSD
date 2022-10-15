9cde95a47d6004729df5b9534345c01e3569885f
Daniel Bahrami <daniel@bahrami.dk>
2022-10-15 21:00:09 +0200

    Set text for movie object

--- Android/MovieDB/app/src/main/java/com/example/moviedb/MovieDetailsActivity.kt
+++ Android/MovieDB/app/src/main/java/com/example/moviedb/MovieDetailsActivity.kt
@@ -2,10 +2,25 @@ package com.example.moviedb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.moviedb.database.MovieEntity
 
 class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+
+        val movie = intent.getSerializableExtra("movies") as MovieEntity
+
+
+        val titleTextView = findViewById<TextView>(R.id.movieReleaseYear).apply {
+            text = movie.movieTitle
+        }
+
+        val releaseYearTextView = findViewById<TextView>(R.id.movieTitle).apply {
+            text = movie.movieReleaseYear
+        }
+
+
     }
 }
