bbedf06d6eaaa83a72e1ad6b506201bed04781e1
Daniel Bahrami <daniel@bahrami.dk>
2022-11-13 22:10:05 +0100

    Add tasks

---
+++ PWA/to-do/src/tasks.ts
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    id: "0",
+    name: "Water the plants",
+    isCompleted: false,
+  },
+  {
+    id: "1",
+    name: "Do laundry",
+    isCompleted: false,
+  },
+  {
+    id: "2",
+    name: "Buy groceries ",
+    isCompleted: true,
+  },
+];
