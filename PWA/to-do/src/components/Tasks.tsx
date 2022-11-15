9024562babd072d66f08a5ce10ca14264cb5d110
Daniel Bahrami <daniel@bahrami.dk>
2022-11-15 18:30:23 +0100

    Remove unused imports

--- PWA/to-do/src/components/Tasks.tsx
+++ PWA/to-do/src/components/Tasks.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, ChangeEvent, useState } from "react";
+import { FormEvent, ChangeEvent, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import Task from "./Task";
