4b14fc1d4a44ed6790f425508455ca98294f3160
Daniel Bahrami <daniel@bahrami.dk>
2022-11-22 23:42:11 +0100

    Use uuid for the task model

--- Flutter/to_do/lib/task_model.dart
+++ Flutter/to_do/lib/task_model.dart
@@ -1,4 +1,12 @@
+import 'package:uuid/uuid.dart';
+
+createId() {
+  const uuid = Uuid();
+  return uuid.v4();
+}
+
 class TaskModel {
+  String id = createId();
   String task;
   bool isCompleted;
 
